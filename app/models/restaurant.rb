72a7437121b5d6ec756c3738deae196195fb67e4
Nadia Salmen <nadiasalmen@gmail.com>
2021-01-11 23:54:31 -0300

    Add validations to Review model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,6 @@ class Restaurant < ApplicationRecord
   validates :address, presence: true
   validates :category, presence: true
   validates :category, inclusion: { in: ['chinese', 'italian', 'japanese', 'french', 'belgian'] }
+
+  has_many :reviews, dependent: :destroy
 end
