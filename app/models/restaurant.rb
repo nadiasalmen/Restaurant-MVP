5727bc05e26fe34651541fee78395d704acbcb18
Nadia Salmen <nadiasalmen@gmail.com>
2021-02-04 12:56:10 -0300

    Add Restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,2 @@
 class Restaurant < ApplicationRecord
-  validates :name, presence: true
-  validates :address, presence: true
-  validates :category, presence: true
-  validates :category, inclusion: { in: ['chinese', 'italian', 'japanese', 'french', 'belgian'] }
-
-  has_many :reviews, dependent: :destroy
 end
--- app/models/review.rb
+++
@@ -1,8 +0,0 @@
-class Review < ApplicationRecord
-  validates :content, presence: true
-  validates :rating, presence: true
-  validates :rating, inclusion: { in: (0..5).to_a, allow_nil: false }
-  validates :rating, numericality: true
-
-  belongs_to :restaurant
-end
