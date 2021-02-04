b8a193b8a525cd854088fd038610945b1507d05c
Nadia Salmen <nadiasalmen@gmail.com>
2021-02-04 15:50:16 -0300

    Add user story: A visitor can see the list of all restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Read all
+  get 'restaurants', to: 'restaurants#index', as: :restaurants
+
+  # Create one
+
+  # Read one - show must go after the "new" routes
+
 end
